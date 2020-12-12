875ad51c491b9daa73ce5c224de10ddf4f84b11a
naro143 <hoshipuri0902@gmail.com>
2020-12-12 16:20:51 +0900

    fix: Twitter

--- components/atoms/Twitter.vue
+++ components/atoms/Twitter.vue
@@ -1,11 +1,23 @@
 <template>
   <div class="Twitter">
     <h2>Twitter</h2>
+    <a href="https://twitter.com/hashtag/DeNA21AdCal?src=hashtag_click">
+      #DeNA21AdCal
+    </a>
+    <a href="https://twitter.com/hashtag/DeNA20AdCal?src=hashtag_click">
+      #DeNA20AdCal
+    </a>
+    <a href="https://twitter.com/hashtag/DeNA_AdCal?src=hashtag_click">
+      #DeNA_AdCal
+    </a>
     <a
-      class="twitter-moment"
-      href="https://twitter.com/i/moments/1333447615128563715?ref_src=twsrc%5Etfw"
+      href="https://twitter.com/DeNAxTech?ref_src=twsrc%5Etfw"
+      class="twitter-follow-button"
+      data-size="large"
+      data-lang="ja"
+      data-show-count="false"
     >
-      Test
+      Follow @DeNAxTech
     </a>
     <script
       async
@@ -26,4 +38,12 @@ export default {
   padding: 0 10px
   h2
     border-bottom: 4px solid $color-primary
+  a
+    display: block
+    margin: 10px 0
+    color: $color-shinsotsu
+    &:first-of-type
+      color: $color-naitei
+    &:last-of-type
+      color: $color-general
 </style>
